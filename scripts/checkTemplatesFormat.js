b0453dc15595ec5ce11a98be4aba726ef83a54ff
ondrejch <ondrejch@kentico.com>
2020-06-26 14:14:28 +0200

    add static format check of the template list JSON file

---
+++ scripts/checkTemplatesFormat.js
@@ -0,0 +1,66 @@
+const Joi = require('@hapi/joi');
+const templates = require('../templates/list.json');
+
+const templateSchema = Joi.object({
+  name: Joi.string().max(60).required(),
+  description: Joi.string().max(250).required(),
+  repoUrl: Joi.string().uri({ scheme: 'https' }).required(),
+  exportPackageUrl: Joi.string().uri({ scheme: 'https' }).required(),
+  imageUrl: Joi.string().uri({ scheme: 'https' }).required(),
+  author: Joi.object({
+    name: Joi.string().max(60).required(),
+    email: Joi.string().email().required()
+  }).required(),
+  lastUpdate: Joi.date().required(),
+  forVersion: Joi.string().regex(/[0-9.]+/).required(),
+  name: Joi.string().max(60).required(),
+  technology: Joi.string().max(40).required(),
+});
+
+const templateValid = (template) => {
+  const result = templateSchema.validate(template);
+
+  if (result.error) {
+    console.error(result.error.message);
+    return false;
+  }
+  return true;
+};
+
+const templateIsValid = () => {
+  if (!Array.isArray(templates)) {
+    console.error('Extensions are not valid array');
+    return false;
+  }
+
+  // template names lowercased extensions' names so we need them to be unique
+  const uniqueResult = Joi
+    .array()
+    .unique((a, b) =>
+      a.name.toLocaleLowerCase() === b.name.toLocaleLowerCase())
+    .validate(templates)
+  if (uniqueResult.error) {
+    console.error(`Multiple templates with same name (case is ignored) are not supported.`);
+    return false;
+  }
+
+  for (const template of templates) {
+    if (!templateValid(template)) {
+      console.log(`Tested template ${template.name ? template.name : JSON.stringify(template, 2)}.`);
+      return false;
+    }
+  }
+
+  console.info('Templates format is valid.')
+  return true;
+};
+
+if (templateIsValid()) {
+  return 0;
+} else {
+  process.exit(1);
+}
+
+module.exports = {
+  templateIsValid,
+}
